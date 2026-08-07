27e7a91d2b231f1e87ddcbe982818773caadfb70
Hamsini242007 <hamshu2007@gmail.com>
2026-08-07 10:16:33 +0530

    Time: 0 ms (100%), Space: 43.7 MB (6.88%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,35 +10,51 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if(head.next ==null) return null;
+        ListNode slow=head, fast=head;
+        for(int i=0;i<n;i++){
+            if(fast !=null) fast=fast.next;
+            else break;
+        }
+        if (fast == null) return head.next;
+        while(fast.next !=null){
+            slow=slow.next;
+            fast=fast.next;
+            
+        }
+        slow.next = slow.next.next;
+        return head;
+
+
+
+    //     if(head.next ==null) return null;
         
-        ListNode start=reverse(head);
-        if(n==1){
-            start= start.next;
-        }else{
-            ListNode curr=start;
-            int i=1;
+    //     ListNode start=reverse(head);
+    //     if(n==1){
+    //         start= start.next;
+    //     }else{
+    //         ListNode curr=start;
+    //         int i=1;
         
-        while(curr!=null && curr.next !=null){
-            if(i==n-1){
-                curr.next=curr.next.next;
-                break;
-            }else{
-                curr=curr.next;
-                i++;
-            }
-        }
-        }
-        return reverse(start);
-    }
-    private ListNode reverse(ListNode start){
-        ListNode curr=start, prev=null;
-        while(curr !=null){
-            ListNode temp=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=temp;
-        }
-        return prev;
+    //     while(curr!=null && curr.next !=null){
+    //         if(i==n-1){
+    //             curr.next=curr.next.next;
+    //             break;
+    //         }else{
+    //             curr=curr.next;
+    //             i++;
+    //         }
+    //     }
+    //     }
+    //     return reverse(start);
+    // }
+    // private ListNode reverse(ListNode start){
+    //     ListNode curr=start, prev=null;
+    //     while(curr !=null){
+    //         ListNode temp=curr.next;
+    //         curr.next=prev;
+    //         prev=curr;
+    //         curr=temp;
+    //     }
+    //     return prev;
     }
 }
