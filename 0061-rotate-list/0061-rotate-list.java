b26f85bfe0c3c936ea32ddc051d5595b5c60ddd5
Hamsini242007 <hamshu2007@gmail.com>
2026-08-12 18:34:45 +0530

    Time: 0 ms (100%), Space: 44.6 MB (10.01%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -14,23 +14,47 @@ class Solution {
         ListNode current=head;
         ListNode dummy= new ListNode(0);
         dummy.next=head;
-        int a,len=0;
+        int len=0;
         while( current!=null){
             len++;
             current=current.next;
         }
         k=k%len;
+        if(k==0) return head;
+        int i=len-k;
         current=head;
-        for(int i=0;i<k;i++){
-            while( current.next!=null && current.next.next != null){
-                current=current.next;
-            }
-            ListNode end=current.next;
-            current.next=null;
-            end.next=dummy.next;
-            dummy.next=end;
-            current=dummy.next;
+        for(int j=0;j<i-1;j++){
+            current=current.next;
         }
+        dummy.next=current.next;
+        ListNode s=current.next;
+        while(s.next!=null){
+            s=s.next;
+        }
+        current.next=null;
+        s.next=head;
         return dummy.next;
+        // if(head==null || head.next==null) return head;
+        // ListNode current=head;
+        // ListNode dummy= new ListNode(0);
+        // dummy.next=head;
+        // int len=0;
+        // while( current!=null){
+        //     len++;
+        //     current=current.next;
+        // }
+        // k=k%len;
+        // current=head;
+        // for(int i=0;i<k;i++){
+        //     while( current.next!=null && current.next.next != null){
+        //         current=current.next;
+        //     }
+        //     ListNode end=current.next;
+        //     current.next=null;
+        //     end.next=dummy.next;
+        //     dummy.next=end;
+        //     current=dummy.next;
+        // }
+        // return dummy.next;
     }
 }
