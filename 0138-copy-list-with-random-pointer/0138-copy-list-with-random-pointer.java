3e3b06022e2e10370b262854ae8f77a364bbc069
Hamsini242007 <hamshu2007@gmail.com>
2026-08-13 17:56:05 +0530

    Time: 0 ms (100%), Space: 46.7 MB (14.72%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,49 +15,49 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        // if (head == null) return null;
-        // Node curr = head;
-        // while (curr != null) {
-        //     Node copy = new Node(curr.val);
-        //     copy.next = curr.next;
-        //     curr.next = copy;
-        //     curr = copy.next;
-        // }
-        // curr = head;
-        // while (curr != null) {
-        //     if (curr.random != null) {
-        //         curr.next.random = curr.random.next;
-        //     }
-        //     curr = curr.next.next;
-        // }
-        // curr = head;
-        // Node newHead = head.next;
-        // Node copyCurr = newHead;
-
-        // while (curr != null) {
-        //     curr.next = curr.next.next;
-        //     if (copyCurr.next != null) {
-        //         copyCurr.next = copyCurr.next.next;
-        //     }
-        //     curr = curr.next;
-        //     copyCurr = copyCurr.next;
-        // }
-
-        // return newHead;
-
         if (head == null) return null;
-        HashMap<Node, Node> map = new HashMap<>();
         Node curr = head;
         while (curr != null) {
-            map.put(curr, new Node(curr.val));
-            curr = curr.next;
+            Node copy = new Node(curr.val);
+            copy.next = curr.next;
+            curr.next = copy;
+            curr = copy.next;
         }
         curr = head;
         while (curr != null) {
-            map.get(curr).next = map.get(curr.next);
-            map.get(curr).random = map.get(curr.random);
-            curr = curr.next;
+            if (curr.random != null) {
+                curr.next.random = curr.random.next;
+            }
+            curr = curr.next.next;
         }
-        return map.get(head);
+        curr = head;
+        Node newHead = head.next;
+        Node copyCurr = newHead;
+
+        while (curr != null) {
+            curr.next = curr.next.next;
+            if (copyCurr.next != null) {
+                copyCurr.next = copyCurr.next.next;
+            }
+            curr = curr.next;
+            copyCurr = copyCurr.next;
+        }
+
+        return newHead;
+
+        // if (head == null) return null;
+        // HashMap<Node, Node> map = new HashMap<>();
+        // Node curr = head;
+        // while (curr != null) {
+        //     map.put(curr, new Node(curr.val));
+        //     curr = curr.next;
+        // }
+        // curr = head;
+        // while (curr != null) {
+        //     map.get(curr).next = map.get(curr.next);
+        //     map.get(curr).random = map.get(curr.random);
+        //     curr = curr.next;
+        // }
+        // return map.get(head);
     }
 }
