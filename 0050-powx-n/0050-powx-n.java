ed15427850bc9dead1ecbcdc6a2bdf6102086ea5
Hamsini242007 <hamshu2007@gmail.com>
2026-08-15 13:04:16 +0530

    Time: 0 ms (100%), Space: 48.4 MB (11.71%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,11 +1,13 @@
 class Solution {
     public double myPow(double x, int n) {
         long N=n;
-        //double ans=1;
+        double ans=1;
         if(n<0){
             x=1/x;
             N=-N;
         }
+        //below commented code is included if i dont want to solve using recursion
+
         // while(N>0){
         //     if(N%2==1){
         //         ans*=x;
@@ -13,18 +15,21 @@ class Solution {
         //     x*=x;
         //     N/=2;
         // }
-        // return ans;
+        //  return ans;
 
-        //return power(x,N,ans);
-        return fastPow(x,N);
+        //2 options for recursion : with and without ans variable
+        
+        return power(x,N,ans);
+        //return fastPow(x,N);
     }
-    // private double power(double x, long N,double ans){
-    //     if(N<=0) return ans;
-    //     if(N%2==1){
-    //             ans*=x;
-    //     }
-    //     return power(x*x,N/2,ans);
-    // }
+    private double power(double x, long N,double ans){
+        if(N<=0) return ans;
+        if(N%2==1){
+                ans*=x;
+        }
+        return power(x*x,N/2,ans);
+    }
+
     private double fastPow(double x, long N) {
         if (N == 0) return 1.0;
 
