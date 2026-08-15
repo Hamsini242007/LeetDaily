a5d101d8b188411d1717a2f734b762ea57ac202b
Hamsini242007 <hamshu2007@gmail.com>
2026-08-15 12:57:04 +0530

    Time: 0 ms (100%), Space: 48.2 MB (35.09%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,7 +1,7 @@
 class Solution {
     public double myPow(double x, int n) {
         long N=n;
-        double ans=1;
+        //double ans=1;
         if(n<0){
             x=1/x;
             N=-N;
@@ -14,13 +14,26 @@ class Solution {
         //     N/=2;
         // }
         // return ans;
-        return power(x,N,ans);
+
+        //return power(x,N,ans);
+        return fastPow(x,N);
     }
-    private double power(double x, long N,double ans){
-        if(N<=0) return ans;
-        if(N%2==1){
-                ans*=x;
+    // private double power(double x, long N,double ans){
+    //     if(N<=0) return ans;
+    //     if(N%2==1){
+    //             ans*=x;
+    //     }
+    //     return power(x*x,N/2,ans);
+    // }
+    private double fastPow(double x, long N) {
+        if (N == 0) return 1.0;
+
+        double half = fastPow(x, N / 2);
+
+        if (N % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
         }
-        return power(x*x,N/2,ans);
     }
 }
