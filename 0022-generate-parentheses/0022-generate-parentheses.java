8a086b01ec17ede7cda60812f11aea1887baf106
Hamsini242007 <hamshu2007@gmail.com>
2026-08-18 14:45:37 +0530

    Time: 1 ms (85.78%), Space: 43.7 MB (99.51%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,10 +1,27 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ans=new ArrayList<>();
-        String curr="";
-        alter(curr,0,0,n,ans);
-        
+        //String curr="";
+        //alter(curr,0,0,n,ans);
+        StringBuilder sb=new StringBuilder();
+        alter(sb,0,0,n,ans);
         return ans;
+    }
+    private void alter(StringBuilder sb,int open,int close,int n,List<String> ans){
+        if(sb.length()==2*n){
+            ans.add(sb.toString());
+            return;
+        }
+        if(open<n){
+            sb.append('(');
+            alter(sb,open+1,close,n,ans);
+            sb.deleteCharAt(sb.length()-1);
+        }
+        if(close<open){
+            sb.append(')');
+            alter(sb,open,close+1,n,ans);
+            sb.deleteCharAt(sb.length()-1);
+        }
     }
     private void alter(String curr,int open,int close,int n,List<String> ans){
         if(curr.length()==2*n){
