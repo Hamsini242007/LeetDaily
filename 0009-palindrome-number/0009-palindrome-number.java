471dabb58b4e0fc0498f0dc0ec72cca3eb684800
Hamsini242007 <hamshu2007@gmail.com>
2026-08-19 18:10:46 +0530

    Time: 5 ms (81.49%), Space: 45.9 MB (53.38%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp=x;
+        int rev=0;
+        if(x==0){return true;}
+        while(temp>0){
+            rev=(rev*10)+(temp%10);
+            temp/=10;
+        }
+        if(rev==x){
+        return true;
+        }else{
+            return false;
+        }
+    }
+}
