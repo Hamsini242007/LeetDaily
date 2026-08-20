1953ec698d4ff961bf09e1ba054024b9afb490d0
Hamsini242007 <hamshu2007@gmail.com>
2026-08-20 15:25:15 +0530

    Time: 716 ms (5.44%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,5 +1,11 @@
-SELECT p.firstName, p.lastName, a.city, a.state 
-FROM Person p 
+#SELECT p.firstName, p.lastName, a.city, a.state 
+#FROM Person p 
 #LEFT JOIN Address a
 #ON p.personId = a.personId;
-LEFT JOIN Address a USING (personId)
+#LEFT JOIN Address a USING (personId)
+SELECT 
+    p.firstName, 
+    p.lastName, 
+    (SELECT city FROM Address a WHERE a.personId = p.personId) AS city,
+    (SELECT state FROM Address a WHERE a.personId = p.personId) AS state
+FROM Person p;
