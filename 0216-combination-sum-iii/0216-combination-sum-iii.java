ceb501b198c549e021fc7c1ac439ac574204e903
Hamsini242007 <hamshu2007@gmail.com>
2026-08-23 12:21:37 +0530

    Time: 0 ms (100%), Space: 42.3 MB (82.85%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -2,7 +2,7 @@ class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> ans =new ArrayList<>();
         List<Integer> arr=new ArrayList<>(k);
-        combination(k, n, ans, arr, 1);
+        combination(k,n,ans,arr,1);
         return ans;
     }
     private void combination(int k, int target, List<List<Integer>> ans, List<Integer> arr, int index){
@@ -12,11 +12,20 @@ class Solution {
             }
             return;
         }
-        for(int i=index;i<10;i++){
+        // using i<=maxIndex instead of i<10
+        int maxIndex=9-(k-arr.size())+1;
+        for(int i=index;i<=maxIndex;i++){
             if(i>target) break;
             arr.add(i);
             combination(k,target-i,ans,arr,i+1);
             arr.remove(arr.size()-1);
         }
     }
+        /*
+        if arr.size() is far from k and there aren't enough remaining numbers left in the loop (from index to 9),
+        your code will still make redundant recursive calls.
+        For example, if k = 5 and arr.size() = 1, but index = 7, you only have numbers 
+        {7, 8, 9} (3 numbers) remaining, but you need 4 more numbers to reach $k=5$.
+        The search is guaranteed to fail, yet the recursion proceeds.
+        */
 }
