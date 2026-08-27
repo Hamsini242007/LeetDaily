7d11e4ac4bc941d3fe504f730fd717f8b03c2e89
Hamsini242007 <hamshu2007@gmail.com>
2026-08-27 15:56:56 +0530

    Time: 0 ms (100%), Space: 9 MB (9.36%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.c
@@ -0,0 +1,19 @@
+char* removeOuterParentheses(char* s) {
+    int len=strlen(s);
+    char* stack=(char*)malloc(len*sizeof(char));
+    if(stack==NULL) return NULL;
+    int top=-1;
+    int opened=0;
+    for(int i=0;i<len;i++){
+        char ch=s[i];
+        if(ch=='('){
+            if (opened>0) stack[++top]=ch;
+            opened++;
+        }else{
+            opened--;
+            if (opened>0) stack[++top]=ch;
+        }
+    }
+    stack[++top]='\0';
+    return stack;
+}
