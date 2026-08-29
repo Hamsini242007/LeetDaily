181aa76c5772e345814927ec57707da4a0e6164b
Hamsini242007 <hamshu2007@gmail.com>
2026-08-29 22:42:32 +0530

    Time: 0 ms (100%), Space: 8.5 MB (77.42%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.c
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.c
@@ -1,10 +1,11 @@
 int maxDepth(char* s) {
     int count =0,max=0;
-    for(int i=0;s[i]!='\0';i++){
-        char ch=s[i];
-        if(ch=='(') count++;
-        else if(ch==')') count--;
-        max=(count>max)?count:max;
+    while(*s){
+        if(*s=='('){
+            count++;
+            max=(count>max)?count:max;
+        }else if(*s==')') count--;
+        s++;
     }
     return max;
 }
