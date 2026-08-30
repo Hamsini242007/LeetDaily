e8086a9c77d67bce3c6c45fcf4558db27a0be197
Hamsini242007 <hamshu2007@gmail.com>
2026-08-30 12:20:04 +0530

    Time: 0 ms (100%), Space: 8.7 MB (63.43%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.c
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.c
@@ -1,14 +1,11 @@
 char* reversePrefix(char* word, char ch) {
-    int len=strlen(word);
     int top=-1;
-    char rev[len];
+    char rev[strlen(word)];
     // char* rev=(char*)malloc(len*sizeof(char));
     // if(rev==NULL) return NULL;
-
     for(int i=0;word[i]!='\0';i++){
-        char l=word[i];
-        rev[++top]=l;
-        if(l==ch){
+        rev[++top]=word[i];
+        if(word[i]==ch){
             for(int j=0;j<i+1;j++){
                 word[j]=rev[top--];
             }
