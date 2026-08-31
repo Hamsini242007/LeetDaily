33f2886384e2eb458ac469af8ef6a9e55561ad58
Hamsini242007 <hamshu2007@gmail.com>
2026-08-31 09:13:34 +0530

    Time: 599 ms (94.48%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,5 +1,15 @@
-DELETE p1
-FROM person p1
-JOIN person p2
-ON p1.email=p2.email
-WHERE p1.id>p2.id;
+-- DELETE p1
+-- FROM person p1
+-- JOIN person p2
+-- ON p1.email=p2.email
+-- WHERE p1.id>p2.id;
+
+DELETE FROM Person 
+WHERE id NOT IN (
+    SELECT min_id 
+    FROM (
+        SELECT MIN(id) AS min_id 
+        FROM Person 
+        GROUP BY email
+    ) AS temp
+);
