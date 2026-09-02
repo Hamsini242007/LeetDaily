b00e8e8d198b2a8731c7dc5fbea8afa3967ad177
Hamsini242007 <hamshu2007@gmail.com>
2026-09-02 13:24:09 +0530

    Time: 3 ms (2.15%), Space: 8.7 MB (11.83%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.c
@@ -0,0 +1,12 @@
+char* clearDigits(char* s) {
+    int len=strlen(s);
+    char* stack=(char*)malloc((len+1)*sizeof(char));
+    if(stack==NULL) return NULL;
+    int top=-1;
+    for(int i=0;i<len;i++){
+        if(isdigit(s[i])) top--;
+        else stack[++top]=s[i];
+    }
+    stack[++top]='\0';
+    return stack;
+}
