7fe0244c79599508e26ee96f48f3361db5313ee8
Hamsini242007 <hamshu2007@gmail.com>
2026-09-04 09:18:01 +0530

    Time: 943 ms (88.97%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -2,5 +2,5 @@ SELECT e.name, b.bonus
 FROM Employee e
 LEFT JOIN Bonus b
 ON e.empId=b.empId
-#WHERE (b.bonus IS NULL) OR (b.bonus<1000);
-WHERE IFNULL(b.bonus,0)<1000;
+WHERE (b.bonus IS NULL) OR (b.bonus<1000);
+#WHERE IFNULL(b.bonus,0)<1000;
