be355c00cbe0ee010c12c77cc51cc3a741c5adda
Hamsini242007 <hamshu2007@gmail.com>
2026-09-05 11:48:58 +0530

    Time: 600 ms (25.66%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,4 +1,4 @@
 SELECT name
 FROM customer
-#WHERE IFNULL(a.referee_id,0) !=2;
-WHERE (referee_id!=2) OR (referee_id IS NULL);
+WHERE IFNULL(referee_id,0) !=2;
+#WHERE (referee_id!=2) OR (referee_id IS NULL);
