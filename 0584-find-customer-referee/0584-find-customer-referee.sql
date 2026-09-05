639e07605065b8978c11091155dbdb2fec6d8f10
Hamsini242007 <hamshu2007@gmail.com>
2026-09-05 11:46:57 +0530

    Time: 1505 ms (5.02%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -2,4 +2,5 @@ SELECT a.name
 FROM customer a
 LEFT JOIN customer b
 ON a.id=b.id
-WHERE IFNULL(a.referee_id,0) !=2;
+#WHERE IFNULL(a.referee_id,0) !=2;
+WHERE (a.referee_id!=2) OR (a.referee_id IS NULL);
