417cab588604cd7375aeb704b689d637860437cf
Hamsini242007 <hamshu2007@gmail.com>
2026-09-07 13:15:26 +0530

    Time: 542 ms (38.69%), Space: 0B (100%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,10 +1,16 @@
-SELECT customer_number
-FROM orders
-GROUP BY customer_number
-HAVING COUNT(order_number)=(
-    SELECT COUNT(order_number)
-    FROM orders
+-- SELECT customer_number
+-- FROM orders
+-- GROUP BY customer_number
+-- ORDER BY COUNT(order_number) DESC
+-- LIMIT 1;
+
+WITH RankedOrders AS (
+    SELECT 
+        customer_number,
+        DENSE_RANK() OVER (ORDER BY COUNT(order_number) DESC) AS rnk
+    FROM Orders
     GROUP BY customer_number
-    ORDER BY COUNT(order_number) DESC
-    LIMIT 1
-);
+)
+SELECT customer_number
+FROM RankedOrders
+WHERE rnk = 1;
