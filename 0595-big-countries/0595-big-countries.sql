c26a0f94a214c448d9b63b6c5fc1b1be7f6c1e69
Hamsini242007 <hamshu2007@gmail.com>
2026-09-08 13:30:00 +0530

    Time: 260 ms (99.7%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,3 +1,7 @@
 SELECT name, population, area
 FROM world
-WHERE population>=25000000 OR area>=3000000;
+WHERE area>=3000000
+UNION
+SELECT name, population, area
+FROM world
+WHERE population>=25000000;
