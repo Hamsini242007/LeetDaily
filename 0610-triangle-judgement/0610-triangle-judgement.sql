cb80a357b6187aa9ed92a558d81b6ba0e98c0654
Hamsini242007 <hamshu2007@gmail.com>
2026-09-11 09:10:29 +0530

    Time: 689 ms (5%), Space: 0B (100%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,5 +1,11 @@
 SELECT x,y,z,
-CASE 
-WHEN x+y>z AND y+z>x AND x+z>y THEN 'Yes' ELSE 'No' 
-END AS triangle
+IF (x+y>z AND y+z>x AND x+z>y, 'Yes','No') AS triangle
+-- CASE 
+-- WHEN x+y>z AND y+z>x AND x+z>y THEN 'Yes' ELSE 'No' 
+-- END AS triangle
 FROM triangle;
+# instead of above conditions i can use this in WHEN
+/*CASE 
+ WHEN (x + y + z) - GREATEST(x, y, z) > GREATEST(x, y, z) THEN 'Yes'
+ ELSE 'No'
+END AS triangle */
