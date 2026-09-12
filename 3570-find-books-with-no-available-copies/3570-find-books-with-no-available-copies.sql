7779551caf74b663cbb4d28d77bd3749c1c8a735
Hamsini242007 <hamshu2007@gmail.com>
2026-09-12 14:05:27 +0530

    Time: 543 ms (80.81%), Space: 0B (100%) - LeetHub

--- 3570-find-books-with-no-available-copies/3570-find-books-with-no-available-copies.sql
+++ 3570-find-books-with-no-available-copies/3570-find-books-with-no-available-copies.sql
@@ -1,7 +1,8 @@
-SELECT l.book_id, l.title, l.author, l.genre, l.publication_year, l.total_copies AS current_borrowers
+SELECT l.book_id, l.title, l.author, l.genre, l.publication_year, COUNT(b.record_id) AS current_borrowers
 FROM library_books l
-JOIN borrowing_records b ON b.book_id=l.book_id
+JOIN borrowing_records b 
+ON b.book_id=l.book_id
 WHERE b.return_date IS NULL
 GROUP BY l.book_id, l.title, l.author, l.genre, l.publication_year, l.total_copies
-HAVING COUNT(b.book_id)=l.total_copies
+HAVING COUNT(b.record_id)=l.total_copies
 ORDER BY current_borrowers DESC, l.title ASC;
