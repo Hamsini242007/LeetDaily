86a029fb6267ae51fe833dbdf977bdf9926fb352
Hamsini242007 <hamshu2007@gmail.com>
2026-09-16 16:52:30 +0530

    Time: 48 ms (65.78%), Space: 55.3 MB (95.98%) - LeetHub

--- 2665-counter-ii/2665-counter-ii.js
+++ 2665-counter-ii/2665-counter-ii.js
@@ -5,15 +5,15 @@
 var createCounter = function(init) {
     let curr=init;
     return {
-        increment(){return ++curr;},
-        decrement(){return --curr;},
-        reset(){
-            curr=init
-            return curr;
-        }
-        // increment:()=>++curr,
-        // decrement:()=>--curr,
-        // reset:()=>(curr=init)
+        // increment(){return ++curr;},
+        // decrement(){return --curr;},
+        // reset(){
+        //     curr=init
+        //     return curr;
+        // }
+        increment:()=>++curr,
+        decrement:()=>--curr,
+        reset:()=>(curr=init)
     };
 };
 /**
