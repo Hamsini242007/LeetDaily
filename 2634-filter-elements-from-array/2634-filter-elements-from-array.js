a41ffb8f1e764043749bcd79e241b7e3a4952939
Hamsini242007 <hamshu2007@gmail.com>
2026-09-18 09:08:28 +0530

    Time: 58 ms (5.17%), Space: 53.8 MB (38.51%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -4,7 +4,7 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    let temp=[];
+    const temp=[];
     for(let i=0;i<arr.length;i++){
         if(fn(arr[i],i)){
             temp.push(arr[i]);
