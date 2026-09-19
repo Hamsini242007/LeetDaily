05c7ae845bc156dd6f47177bd9b8dffcf6f117b1
Hamsini242007 <hamshu2007@gmail.com>
2026-09-19 09:04:47 +0530

    Time: 54 ms (12.36%), Space: 53.7 MB (75.64%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -8,5 +8,8 @@ var reduce = function(nums, fn, init) {
     let temp=init;
     for(let i=0;i<nums.length;i++)
         temp=fn(temp,nums[i]);
+    // for(const num of nums){
+    //     temp=fn(temp,num);
+    // }
     return temp;
 };
