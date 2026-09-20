602cd7bd49c3baacbdf6fde4e54ea3ce4c8d3692
Hamsini242007 <hamshu2007@gmail.com>
2026-09-20 18:56:52 +0530

    Time: 47 ms (88.18%), Space: 55.5 MB (15.16%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -5,11 +5,12 @@
 var compose = function(functions) {
     
     return function(x) {
-        for(let i=functions.length-1;i>=0;i--){
-           var f=functions[i];
-           x=f(x);
-        }
-        return x;
+        // for(let i=functions.length-1;i>=0;i--){
+        //    var f=functions[i];
+        //    x=f(x);
+        // }
+        // return x;
+        return functions.reduceRight((acc, f) => f(acc), x);
     }
 };
 
