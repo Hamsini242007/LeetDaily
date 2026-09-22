103b9b399e992464f5fd8455b4ccc308c25d8e07
Hamsini242007 <hamshu2007@gmail.com>
2026-09-22 13:33:53 +0530

    Time: 43 ms (60.28%), Space: 53.4 MB (66.2%) - LeetHub

--- 2666-allow-one-function-call/2666-allow-one-function-call.js
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -4,14 +4,14 @@
  */
 var once = function(fn) {
     let called=false;
-    // return function(...args){
-    //     if(!called){
-    //         called=true;
-    //         return fn(...args);
-    //     }
-    //     return undefined;
-    // }
-    return (...args)=>called?undefined:(called=true, fn(...args));
+    return function(...args){
+        if(!called){
+            called=true;
+            return fn(...args);
+        }
+        return undefined;
+    }
+    // return (...args)=>called?undefined:(called=true, fn(...args));
 };
 
 /**
