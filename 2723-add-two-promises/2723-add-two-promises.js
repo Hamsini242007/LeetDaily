929f57cc999fdc982053b2aa96115c148260f437
Hamsini242007 <hamshu2007@gmail.com>
2026-09-23 08:18:09 +0530

    Time: 53 ms (58.86%), Space: 53.8 MB (77.48%) - LeetHub

--- 2723-add-two-promises/2723-add-two-promises.js
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -4,13 +4,14 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    // const val1= await promise1;
-    // const val2= await promise2;
+    const val1= await promise1; //serial
+    const val2= await promise2;
 
-    // const[val1,val2]=await Promise.all([promise1,promise2]);
+    // const[val1,val2]=await Promise.all([promise1,promise2]); //parallel
 
-    // return val1+val2;
-    return (await Promise.all([promise1,promise2])).reduce((acc,curr)=> acc+curr);
+    return val1+val2;
+
+    // return (await Promise.all([promise1,promise2])).reduce((acc,curr)=> acc+curr);
 };
 
 /**
